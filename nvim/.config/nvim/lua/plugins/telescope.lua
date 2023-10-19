4612f3e9f3f6512731de618cb17e232620d34879
manisha <deepakbhusari@yahoo.com>
2023-10-19 07:34:03 -0400

    settings modified

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,8 +2,19 @@ local function setup()
     local telescope = require("telescope")
     -- import telescope actions safely
     local actions = require("telescope.actions")
+    local telescope_actions = require("telescope.actions.set")
     -- import telescope-ui-select safely
     local themes = require("telescope.themes")
+local fixfolds ={
+hidden=true,
+attach_mappings=function (_)
+  telescope_actions.select:enhance({
+   post= function() vim.cmd(":normal!zx") end
+})
+return true
+end
+}
+
 
  telescope.setup ({
        defaults = {
@@ -48,9 +59,15 @@ local function setup()
 
     },
     pickers = {
+        buffers=fixfolds,
         find_files = {
-          find_command = { "rg", "--files", "--hidden", "--glob", "!.git", "--glob", "!.stack-work" },
+        --  find_command = { "rg", "--files", "--hidden", "--glob", "!.git", "--glob", "!.stack-work" },
+        fixfolds
         },
+        git_files=fixfolds,
+        grep_string=fixfolds,
+        live_grep=fixfolds,
+        oldfiles=fixfolds,
     },
   },
   extensions = {
@@ -62,10 +79,34 @@ local function setup()
 
    -- telescope.load_extension("fzf")
     telescope.load_extension("ui-select")
+    telescope.load_extension("dap")
 
 
         -- set keymaps
     local keymap = vim.keymap -- for conciseness
+
+     -- nvim-telescope/telescope.nvim
+_G.telescope_find_files_in_path = function(path)
+    local _path = path or vim.fn.input("Dir: ", "", "dir")
+    require("telescope.builtin").find_files({search_dirs = {_path}})
+end
+_G.telescope_live_grep_in_path = function(path)
+    local _path = path or vim.fn.input("Dir: ", "", "dir")
+    require("telescope.builtin").live_grep({search_dirs = {_path}})
+end
+_G.telescope_files_or_git_files = function()
+    local utils = require('telescope.utils')
+    local builtin = require('telescope.builtin')
+    local _, ret, _ = utils.get_os_command_output({
+        'git', 'rev-parse', '--is-inside-work-tree'
+    })
+    if ret == 0 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end
+
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" }) -- find files within current working directory, respects .gitignore
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" }) -- find previously opened files
@@ -84,8 +125,10 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { 'nvim-lua/plenary.nvim' ,
     { 'nvim-telescope/telescope-fzf-native.nvim', 
-        "nvim-telescope/telescope-ui-select.nvim",
     "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-dap.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
 
     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     },
