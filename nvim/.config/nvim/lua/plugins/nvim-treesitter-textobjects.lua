11824b1e3c68d535b616b669b8ca745b185298eb
manisha <deepakbhusari@yahoo.com>
2023-10-19 07:36:21 -0400

    plugins added noice,formatter,nvim-treesitter,nvim-treesitter-textobjects

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,45 @@
+local function setup()
+  --local telescope = require 'nvim-treesitter-textobjects'
+  --local telescope = require 'nvim-treesitter'
+  -- import telescope actions safely
+  --local actions = require 'telescope.actions'
+  -- import telescope-ui-select safely
+  --local themes = require 'telescope.themes'
+  local tr_configs = require 'nvim-treesitter.configs'
+
+  -- require
+  tr_configs.setup {
+    -- ('nvim-treesitter.configs')
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+          ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+        },
+        selection_modes = {
+          ['@parameter.outer'] = 'v',
+          ['@function.outer'] = 'V',
+          ['@class.outer'] = '<c-v>',
+        },
+      },
+    },
+  }
+  -- set keymaps
+  local keymap = vim.keymap -- for conciseness
+
+  --    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" }) -- find files within current working directory, respects .gitignore
+  --    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" }) -- find previously opened files
+end
+
+return {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  after = 'nvim-treesitter',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = setup,
+  --cmd = { "Telescope" }
+}
---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,35 @@
+
+local function setup()
+  local tr_configs = require 'nvim-treesitter.configs'
+
+  -- require
+  tr_configs.setup {
+    -- ('nvim-treesitter.configs')
+    ensure_installed={"c","java","javascript", "lua", "vim", "vimdoc", "query", "python","go","typescript"},
+    highlight = {
+      select = {
+        enable = true,
+      },
+        ident = {
+          enable=true,
+        },
+      },
+    }
+  -- set keymaps
+  --local keymap = vim.keymap -- for conciseness
+
+  vim.opt.foldmethod="expr"
+--  vim.opt.foldexpre="nvim_treesitter#foldexpr()"
+  --    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" }) -- find files within current working directory, respects .gitignore
+  --    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" }) -- find previously opened files
+end
+
+return {
+    'nvim-treesitter/nvim-treesitter', 
+     build=':TSUpdate',
+  dependencies = { },
+  config = setup,
+  --cmd = { "Telescope" }
+}
+
+
