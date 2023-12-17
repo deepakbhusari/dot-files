f18d009a9520709900a757b52f1e8cd05564c487
manisha <deepakbhusari@yahoo.com>
2023-12-17 14:20:03 -0500

    stow config files script

--- script-stow-config-files.sh
+++ script-stow-config-files.sh
@@ -7,6 +7,7 @@ stow -t ~ -v nvim
 stow -t ~ -v vim
 stow -t ~ -v starship
 stow -t ~ -v skhd
+stow -t ~ -v tmux
 stow -t ~ -v yabai
 stow -t ~ -v kitty
 #This will copy files to /etc/data folder (-t option)
