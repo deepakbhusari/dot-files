ff13e5bf46bead5618783c4ec8be72ad29f55f14
manisha <deepakbhusari@yahoo.com>
2024-01-10 22:17:20 -0500

    tokyonight theme

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -4,10 +4,13 @@
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-vim.g.tokyonight_style = 'storm'
+--vim.g.tokyonight_style = 'storm'
+--vim.g.tokyonight_style = 'moon'
+--vim.g.tokyonight_style = 'night'
+--vim.g.tokyonight_style = 'day'
 vim.g.tokyonight_enable_italic=1
 
-      vim.cmd([[colorscheme tokyonight]])
+     vim.cmd([[colorscheme tokyonight-moon]])
     end,
   }
 
