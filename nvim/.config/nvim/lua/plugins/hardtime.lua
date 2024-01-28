c215f256555be7d836713798483f2336cf95b66b
manisha <deepakbhusari@yahoo.com>
2024-01-28 09:59:34 -0500

    hardtime configuration for ctrl-p

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,18 @@
 return {
    "m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = {}
+   opts = 
+   {
+      restricted_keys = {
+  ["<C-P>"] = { },
+      },
+     -- Add "oil" to the disabled_filetypes
+disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+   disabled_keys = {
+   ["<Up>"] = {},
+   ["<C-P>"] = {},
+   ["<Space>"] = { "n", "x" },
+     },
+   }
 }
 
