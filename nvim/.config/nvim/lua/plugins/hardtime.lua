2e1feb6ef53789777131662342e9ace60b12c4fd
manisha <deepakbhusari@yahoo.com>
2024-01-30 19:22:25 -0500

    hardtime, gitsigns plugin setup

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -1,19 +1,18 @@
 -- lazy.nvim
 return {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   opts = 
-   {
-      restricted_keys = {
-  ["<C-P>"] = { },
-      },
-     -- Add "oil" to the disabled_filetypes
-disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
-   disabled_keys = {
-   ["<Up>"] = {},
-   ["<C-P>"] = {},
-   ["<Space>"] = { "n", "x" },
-     },
-   }
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts =
+  {
+    restricted_keys = {
+      ["<C-P>"] = {},
+    },
+    -- Add "oil" to the disabled_filetypes
+    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+    disabled_keys = {
+      ["<Up>"] = {},
+      ["<C-P>"] = {},
+      --   ["<Space>"] = { "n", "x" },
+    },
+  }
 }
-
