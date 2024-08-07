8240cb1e9500871bb3084f002a50c17d141ba029
manisha <deepakbhusari@yahoo.com>
2024-08-07 09:06:07 -0400

    plugin zbirenbaum/copilot.lua changed to github/copilot.vim

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,4 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
-}
+  "github/copilot.vim",
+  }
 
