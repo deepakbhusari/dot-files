5e1ffd35934abbe02a39473978e3f66fb6f16e70
manisha <deepakbhusari@yahoo.com>
2024-09-30 22:56:07 -0400

    plugins lua file

--- nvim/.config/nvim/lua/config/plugins_lazy.lua
+++ nvim/.config/nvim/lua/config/plugins_lazy.lua
@@ -11,11 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.o.termguicolors = true
+
 require('lazy').setup {
   spec = {
     {
       'LazyVim/LazyVim',
       import = 'lazyvim.plugins',
+      -- import = 'plugins',
+      -- import = 'plugins.lsp',
       opts = {
         colorscheme = 'nightfly',
         news = {
@@ -24,34 +29,52 @@ require('lazy').setup {
         },
       },
     },
+
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
     -- { import = 'lazyvim.plugins.extras.coding.copilot' },
-    { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+    -- { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
     { import = 'plugins' },
-    { import = 'plugins.lsp'},
+    { import = 'plugins.lsp' },
   },
+  --{ import = 'lazyvim.plugins.extras.ui.mini-animate' },
+
   defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    version = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   dev = {
-    -- path = '~/.ghq/github.com',
+    path = '~/.ghq/github.com',
   },
-  checker = { enabled = true },
+  -- install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
+  -- install = { colorscheme = { 'nightfly','catppuccin', 'tokyonight' } },
+  -- install = { colorscheme = { 'nightfly', 'catppuccin' } },
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    notify = false, -- don't notify
+  },
+  change_detection = {
+    notify = false,
+  },
   performance = {
     cache = {
       enabled = true,
     },
     rtp = {
+      -- disable some rtp plugins
       disabled_plugins = {
         'gzip',
         -- "matchit",
         -- "matchparen",
-        -- 'netrwPlugin',
+        -- "netrwPlugin",
         'rplugin',
         'tarPlugin',
         'tohtml',
