0c5c16a31555772c6b8f860d75ca8d1a0bdbb1f0
manisha <deepakbhusari@yahoo.com>
2024-09-30 21:08:07 -0400

    lsp config lua file modified

--- nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,210 +1,219 @@
 return {
-   {
-  -- Setup Mason and Mason-Config.
-  'williamboman/mason.nvim',
-  dependencies = {
---    'williamboman/mason-lspconfig.nvim',
---    'jayp0521/mason-null-ls.nvim',
-  },
-  config = function()
-    -- import mason plugin safely
-    local mason = require 'mason'
+  {
+    -- Setup Mason and Mason-Config.
+    'williamboman/mason.nvim',
+    dependencies = {
+      --    'williamboman/mason-lspconfig.nvim',
+      --    'jayp0521/mason-null-ls.nvim',
+    },
+    config = function()
+      -- import mason plugin safely
+      local mason = require 'mason'
 
-    -- import mason-lspconfig plugin safely
-    --local mason_lspconfig = require 'mason-lspconfig'
+      -- import mason-lspconfig plugin safely
+      --local mason_lspconfig = require 'mason-lspconfig'
 
-    -- import mason-null-ls plugin safely
---    local mason_null_ls = require 'mason-null-ls'
+      -- import mason-null-ls plugin safely
+      --    local mason_null_ls = require 'mason-null-ls'
 
-    -- enable mason
-    mason.setup()
-    --require("mason").setup()
+      -- enable mason
+      mason.setup()
+      --require("mason").setup()
 
-   -- require("mason-lspconfig").setup {
+      -- require("mason-lspconfig").setup {
       -- list of servers for mason to install
-   --   ensure_installed = {
-     --   'stylua',
-     --   'selene',
-		--		"luacheck",
-		--		"shellcheck",
-		--		"shfmt",
+      --   ensure_installed = {
+      --   'stylua',
+      --   'selene',
+      --		"luacheck",
+      --		"shellcheck",
+      --		"shfmt",
       --  'svelte',
 
-    --  },
+      --  },
       -- auto-install configured servers (with lspconfig)
-     -- automatic_installation = true, -- not the same as ensure_installed
-    --}
+      -- automatic_installation = true, -- not the same as ensure_installed
+      --}
 
-    --mason_null_ls.setup {
+      --mason_null_ls.setup {
       -- list of formatters & linters for mason to install
-    --  ensure_installed = {
-    --    'prettier', -- ts/js formatter
-    --    'stylua', -- lua formatter
-    --    'eslint_d', -- ts/js linter
-    --  },
+      --  ensure_installed = {
+      --    'prettier', -- ts/js formatter
+      --    'stylua', -- lua formatter
+      --    'eslint_d', -- ts/js linter
+      --  },
       -- auto-install configured servers (with lspconfig)
-   --   automatic_installation = true,
-   -- }
-  end,
+      --   automatic_installation = true,
+      -- }
+    end,
   },
   {
     'williamboman/mason-lspconfig.nvim',
     keys = {
-      {"<leader>i", "<cmd>LspInfo<cr>", "Lsp Info"},
-      {"<leader>I", "<cmd>LspInstall<cr>", "Lsp Install"},
+      { '<leader>i', '<cmd>LspInfo<cr>', 'Lsp Info' },
+      { '<leader>I', '<cmd>LspInstall<cr>', 'Lsp Install' },
     },
-      config=function()
-        require("mason-lspconfig").setup {
-          -- list of servers for mason to install
-          ensure_installed = {
-       --   'stylua',
-       --   'selene',
-				--  'tailwindcss-language-server',
-				--  'typescript-language-server',
-       -- 'css-lsp',
-            'tsserver',
-            'lua_ls',
-            'html',
-            'cssls',
-            'tailwindcss',
-            'graphql',
-            'emmet_ls',
-            'prismals',
-            'clangd',
-            'pyright',
-            'rust_analyzer'
-           },
-           automatic_installation = true,
-          handlers = {
+    config = function()
+      require('mason-lspconfig').setup {
+        -- list of servers for mason to install
+        ensure_installed = {
+          --   'stylua',
+          --   'selene',
+          --  'tailwindcss-language-server',
+          --  'typescript-language-server',
+          -- 'css-lsp',
+          'ts_ls',
+          'lua_ls',
+          'html',
+          'cssls',
+          'tailwindcss',
+          'graphql',
+          'emmet_ls',
+          'prismals',
+          'clangd',
+          'pyright',
+          'rust_analyzer',
+        },
+        automatic_installation = true,
+        handlers = {
           --lsp_zero.default_setup,
-         -- lua_ls = function()
-         -- local lua_opts = lsp_zero.nvim_lua_ls()
+          -- lua_ls = function()
+          -- local lua_opts = lsp_zero.nvim_lua_ls()
           --require('lspconfig').lua_ls.setup(lua_opts)
-       -- end,
-         },
+          -- end,
+        },
       }
-      end,
-     },
-     { -- LSP Support
---    'VonHeikemen/lsp-zero.nvim',
---    branch = 'v3.x',
---    lazy = true,
---    config = false,
---    keys = {
---      {"<leader>i", "<cmd>LspInfo<cr>", "Lsp Info"},
---      {"<leader>I", "<cmd>LspInstall<cr>", "Lsp Install"},
---    },
---    dependencies = {
---      'williamboman/mason.nvim',
---      'williamboman/mason-lspconfig.nvim',
---    },
---    config=function()
-      -- Use lsp_zero to manage lsp attachments.
---      local lsp_zero = require('lsp-zero')
+    end,
+  },
 
---      lsp_zero.on_attach(function(_, bufnr)
---      lsp_zero.default_keymaps({buffer = bufnr})
---      end)
+  { -- LSP Support
+    --    'VonHeikemen/lsp-zero.nvim',
+    --    branch = 'v3.x',
+    --    lazy = true,
+    --    config = false,
+    --    keys = {
+    --      {"<leader>i", "<cmd>LspInfo<cr>", "Lsp Info"},
+    --      {"<leader>I", "<cmd>LspInstall<cr>", "Lsp Install"},
+    --    },
+    --    dependencies = {
+    --      'williamboman/mason.nvim',
+    --      'williamboman/mason-lspconfig.nvim',
+    --    },
+    --    config=function()
+    -- Use lsp_zero to manage lsp attachments.
+    --      local lsp_zero = require('lsp-zero')
 
---     require('mason').setup({})
---        require('mason-lspconfig').setup({
---          ensure_installed = {
---            'tsserver', 
---            'lua_ls',
---            'html',
---            'cssls',
---            'tailwindcss',
---            'graphql',
---            'emmet_ls',
---            'prismals',
---            'clangd', 
---            'pyright', 
---            'rust_analyzer'},
---          handlers = {
---            lsp_zero.default_setup,
---          },
---        })
---    end,
+    --      lsp_zero.on_attach(function(_, bufnr)
+    --      lsp_zero.default_keymaps({buffer = bufnr})
+    --      end)
 
+    --     require('mason').setup({})
+    --        require('mason-lspconfig').setup({
+    --          ensure_installed = {
+    --            'tsserver',
+    --            'lua_ls',
+    --            'html',
+    --            'cssls',
+    --            'tailwindcss',
+    --            'graphql',
+    --            'emmet_ls',
+    --            'prismals',
+    --            'clangd',
+    --            'pyright',
+    --            'rust_analyzer'},
+    --          handlers = {
+    --            lsp_zero.default_setup,
+    --          },
+    --        })
+    --    end,
   },
 
   { -- LSP Config
     'neovim/nvim-lspconfig',
     dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
+      { 'hrsh7th/cmp-nvim-lsp' },
     },
-    config=function()
-     local lspconfig=require("lspconfig")
-     lspconfig.lua_ls.setup({})
-     vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-     vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
-     vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
-     lspconfig.tsserver.setup({})
+    config = function()
+      local lspconfig = require 'lspconfig'
+      lspconfig.lua_ls.setup {}
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+       lspconfig.ts_ls.setup {}
     end,
-
   },
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
-      {'L3MON4D3/LuaSnip'}
+      { 'L3MON4D3/LuaSnip' },
     },
   },
-   { -- Use which-key to show lsp_zero default_keymaps
+
+  { -- Use which-key to show lsp_zero default_keymaps
     'folke/which-key.nvim',
-    event = "VeryLazy",
+    event = 'VeryLazy',
     config = function()
-      require('which-key').setup(
-      {plugins = {spelling = true}}
-      )
+      require('which-key').setup { plugins = { spelling = true } }
     end,
   },
 
   { -- Vim/NeoVim/Tmux seamless navigation.
-    "christoomey/vim-tmux-navigator",
-    event =  "VeryLazy",
+    'christoomey/vim-tmux-navigator',
+    event = 'VeryLazy',
     keys = {
-      {"<c-h>", "<cmd>TmuxNavigateLeft<cr>", "Navigate Window Left"},
-      {"<c-j>", "<cmd>TmuxNavigateDown<cr>", "Navigate Window Down"},
-      {"<c-k>", "<cmd>TmuxNavigateUp<cr>", "Navigate Window Up"},
-      {"<c-l>", "<cmd>TmuxNavigateRight<cr>", "Navigate Window Right"},
+      { '<c-h>', '<cmd>TmuxNavigateLeft<cr>', 'Navigate Window Left' },
+      { '<c-j>', '<cmd>TmuxNavigateDown<cr>', 'Navigate Window Down' },
+      { '<c-k>', '<cmd>TmuxNavigateUp<cr>', 'Navigate Window Up' },
+      { '<c-l>', '<cmd>TmuxNavigateRight<cr>', 'Navigate Window Right' },
     },
   },
-   { -- File navigation
-    "nvim-neo-tree/neo-tree.nvim",
+
+  { -- File navigation
+    'nvim-neo-tree/neo-tree.nvim',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim"
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
     },
     keys = {
       -- {"<c-n>", "<cmd>Neotree toggle<cr>"},
-      {"<leader>nt", "<cmd>Neotree focus<cr>"},
+      { '<leader>nt', '<cmd>Neotree focus<cr>' },
     },
 
     opts = function(_, opts)
       opts.filesystem = {
         bind_to_cwd = false,
-        follow_current_file = { enabled = true}
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        follow_current_file = { enabled = true },
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+        },
       }
-      opts.window = { position = "right", }
+      opts.window = { position = 'left' }
     end,
   },
 
   {
---    'williamboman/mason-lspconfig.nvim',
---      config=function()
---    require('mason-lspconfig').lua_ls.setup({})
---    end,
--- Setup Mason and Mason-Config.
---require('mason').setup({})
---require('mason-lspconfig').setup({
---  handlers = {
---    lsp_zero.default_setup,
---    lua_ls = function()
---      local lua_opts = lsp_zero.nvim_lua_ls()
---      require('lspconfig').lua_ls.setup(lua_opts)
---    end,
---  }),
-   },
+    --    'williamboman/mason-lspconfig.nvim',
+    --      config=function()
+    --    require('mason-lspconfig').lua_ls.setup({})
+    --    end,
+    -- Setup Mason and Mason-Config.
+    --require('mason').setup({})
+    --require('mason-lspconfig').setup({
+    --  handlers = {
+    --    lsp_zero.default_setup,
+    --    lua_ls = function()
+    --      local lua_opts = lsp_zero.nvim_lua_ls()
+    --      require('lspconfig').lua_ls.setup(lua_opts)
+    --    end,
+    --  }),
+    --  },
+  },
 }
 
