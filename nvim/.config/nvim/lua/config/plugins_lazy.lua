f7c1f874851719846eb85bc59ee5282a3b0d8fdc
Deepak Bhusari <deepakbhusari@yahoo.com>
2024-11-03 20:57:39 -0500

    commented lazyvim.plugins

--- nvim/.config/nvim/lua/config/plugins_lazy.lua
+++ nvim/.config/nvim/lua/config/plugins_lazy.lua
@@ -18,7 +18,8 @@ require('lazy').setup {
   spec = {
     {
       'LazyVim/LazyVim',
-      import = 'lazyvim.plugins',
+      -- import = 'lazyvim.plugins',
+
       -- import = 'plugins',
       -- import = 'plugins.lsp',
       opts = {
@@ -31,8 +32,8 @@ require('lazy').setup {
     },
 
     { import = 'lazyvim.plugins.extras.linting.eslint' },
-    { import = 'lazyvim.plugins.extras.formatting.prettier' },
-    { import = 'lazyvim.plugins.extras.lang.typescript' },
+    --    { import = 'lazyvim.plugins.extras.formatting.prettier' },
+    --    { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
     -- { import = 'lazyvim.plugins.extras.coding.copilot' },
